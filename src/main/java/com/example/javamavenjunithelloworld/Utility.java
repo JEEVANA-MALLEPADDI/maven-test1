b2606e559554dee047b707e4767056060beb831d
Jeevana <jeevanacse@gmail.com>
2024-09-16 17:51:13 +0530

    JEEVANAM0001 - Wrote utility function

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,15 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+
+    public static boolean isEvenorOdd(int number) {
+        if(number % 2 == 0) return "even";
+       return "Odd";
+    }
+
+   
+
+    public static void jeevana() {
+        System.out.println("I am Jeevana");
+    }
+}
